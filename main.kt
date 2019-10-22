a8056f7d67084facd17ba54f4777816c69b360cb
Kaden Gordon <kaden.m.gordon@gmail.com>
2019-10-21 20:55:58 -0600

    Fix minor bug, compiles now

--- main.kt
+++ main.kt
@@ -94,18 +94,17 @@ val board6 =
 
 val boards = listOf(board0, board1, board2, board3, board4, board5, board6)
 fun main(args: Array<String>) {
-    //for(board in boards) println(board) //to print all boards at the beginning
+    //for(board in boards) println(board) //to print all boards
     val dict = File("words.txt").useLines { it.toList() }
     val secret = dict.get((0..7699).random())
     val length = secret.length
     println(boards.get(0) + "\n\n")
     for (i: Int in (1..length)) print("_____  ")
-    var guess: String
+    var guess: String?
     var incorrect_guesses = 0
     var correct_guesses = 0
     while(incorrect_guesses<6 && correct_guesses<length) {
         print("\nGuess a letter: ")
         guess = readLine()
     }
-    
 }
